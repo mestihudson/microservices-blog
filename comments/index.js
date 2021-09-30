3576fe7b4f0794aaf46c04e6df780357f6a893a8
mestihudson <mestihudson@gmail.com>
2021-09-30 14:54:39 -0300

    feat: add event receive acknowledgement

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -29,6 +30,11 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments)
 })
 
+app.post('/events', (req, res) => {
+  console.log('Event Received: ' + req.body.type)
+
+  res.send({})
+})
 
 app.listen(4001, () => {
   console.log('Listening on 4001')
--- posts/index.js
+++ posts/index.js
@@ -24,6 +24,11 @@ app.post('/posts', async (req, res) => {
   res.status(201).send(posts[id])
 })
 
+app.post('/events', (req, res) => {
+  console.log('Event Received: ' + req.body.type)
+
+  res.send({})
+})
 
 app.listen(4000, () => {
   console.log('Listening on 4000')
