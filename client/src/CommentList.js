5ba8e3b80eb722f4b2a92a3a61d1d4b03023644d
mestihudson <mestihudson@gmail.com>
2021-09-30 15:08:34 -0300

    feat: change client to use query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,4 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([])
-
-  const fetchData = async () => {
-    const res = await axios
-      .get(`http://localhost:4001/posts/${postId}/comments`)
-
-    setComments(res.data)
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
     return (
       <li key={comment.id}>{comment.content}</li>
