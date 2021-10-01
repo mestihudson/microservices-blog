0fdbda5c6d15bfa270d1765f8a9303fcf36ad78a
mestihudson <mestihudson@gmail.com>
2021-10-01 16:49:04 -0300

    feat: add handling to comment moderated event

--- comments/index.js
+++ comments/index.js
@@ -34,8 +34,23 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event Received: ' + req.body.type)
+
+  const { type, data } = req.body
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data
+    const comments = commentsByPostId[postId]
+
+    const comment = comments.find(comment => comment.id === id)
+    comment.status = status
+
+    await axios.post('http://event-bus:4005/events', {
+      type: 'CommentUpdated',
+      data: { id, status, postId, content }
+    })
+  }
 
   res.send({})
 })
