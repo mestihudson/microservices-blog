920ce15e1e67cd63a86bb953fbd710aaddc6a931
mestihudson <mestihudson@gmail.com>
2021-10-01 16:47:00 -0300

    feat: add handling to comment updated event

--- query/index.js
+++ query/index.js
@@ -28,6 +28,15 @@ app.post('/events', (req, res) => {
     post.comments.push({ id, content, status })
   }
 
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data
+
+    const post = posts[postId]
+    const comment = post.comments.find(comment => comment.id === id)
+    comment.status = status
+    comment.content = content
+  }
+
   console.log(posts)
 
   res.send({})
