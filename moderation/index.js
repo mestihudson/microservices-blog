f7dd3db5f5360c31bb3ee2b4880c99d94ddf1a99
mestihudson <mestihudson@gmail.com>
2021-10-01 16:45:22 -0300

    feat: add handling to comment created event

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,26 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/events', (req, res) => {})
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange')
+      ? 'rejected' : 'approved'
+
+    await axios.post('http://event-bus:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    })
+  }
+
+  res.send({})
+})
 
 app.listen(4003, () => {
   console.log('Listening on 4003')
