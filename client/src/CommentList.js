9222043cb5eb2d1804c06d64d5ff5d557001dea7
mestihudson <mestihudson@gmail.com>
2021-10-01 16:50:30 -0300

    feat: add presented content handling by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,7 +1,15 @@
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
+    const content = comment.status === 'approved'
+      ? comment.content
+      : comment.status === 'pending'
+        ? 'This comment is awaiting moderation'
+        : comment.status === 'rejected'
+          ? 'This comment has been rejected'
+          : ''
+
     return (
-      <li key={comment.id}>{comment.content}</li>
+      <li key={comment.id}>{content}</li>
     )
   })
 
