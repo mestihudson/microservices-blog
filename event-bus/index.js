8f4ebb6e8bae308bcd361aca63a8b171ecee0626
mestihudson <mestihudson@gmail.com>
2021-10-02 12:08:04 -0300

    feat: add database to persiste events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req, res) => {
   const event = req.body
+
+  events.push(event)
 
   axios.post('http://posts:4000/events', event)
   axios.post('http://comments:4001/events', event)
@@ -14,6 +18,10 @@ app.post('/events', (req, res) => {
   axios.post('http://moderation:4003/events', event)
 
   res.send({ status: 'OK' })
+})
+
+app.get('/events', (req, res) => {
+  res.send(events)
 })
 
 app.listen(4005, () => {
