71ccb6151e18e5c74ccb1fac04ac0c0b5a2e63b3
mestihudson <mestihudson@gmail.com>
2021-10-02 12:10:37 -0300

    refactor: move event handling to specific method

--- query/index.js
+++ query/index.js
@@ -8,13 +8,7 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data
 
@@ -36,7 +30,16 @@ app.post('/events', (req, res) => {
     comment.status = status
     comment.content = content
   }
+}
 
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body
+
+  handleEvent(type, data)
 
   res.send({})
 })
