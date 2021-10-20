f4501d69820f9616c8e6d7e9003f38c9be34c8b9
mestihudson <mestihudson@gmail.com>
2021-10-20 00:24:28 -0300

    chore: change endpoint to create posts

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault()
 
-    await axios.post('http://posts.com/posts', {
+    await axios.post('http://posts.com/posts/create', {
       title
     })
 
