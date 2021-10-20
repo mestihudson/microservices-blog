406bb4f50083e708edf5ccbaa144b96edfcfa100
mestihudson <mestihudson@gmail.com>
2021-10-19 22:17:22 -0300

    chore: change service names of services url to promote each other communication

--- posts/index.js
+++ posts/index.js
@@ -20,7 +20,7 @@ app.post('/posts', async (req, res) => {
 
   posts[id] = { id, title }
 
-  await axios.post('http://event-bus:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: { id, title }
   })
